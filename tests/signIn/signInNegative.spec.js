6826a940a263d4cfbfd3050c7f54a79402d80c6f
Inna Ianko <inna.i@mate.academy>
2024-10-21 16:05:07 +0300

    Updated sign in test

--- tests/signIn/signInNegative.spec.js
+++ tests/signIn/signInNegative.spec.js
@@ -1,27 +1,31 @@
 import { test, expect } from '@playwright/test';
 
-test.beforeEach( async ({ page }) => {
-  await page.goto('https://conduit.mate.academy/user/login');
-});
+test.describe('Sign in negative tests', () => {
 
-test('Assert error message for empty password', async ({ page }) => {
- await page.getByPlaceholder('Email').fill('test@gmail.com');
- await page.getByRole('button', { name: 'Sign in' }).click();
+  test.beforeEach( async ({ page }) => {
+    await page.goto('https://conduit.mate.academy/user/login');
+  });
 
- await expect(page.getByRole('list').nth(1)).toContainText(`password:can\'t be blank`);
-});
-
-test('Assert error message for empty email', async ({ page }) => {
-  await page.getByPlaceholder('Password').fill('newpass123!');
-  await page.getByRole('button', { name: 'Sign in' }).click();
- 
-  await expect(page.getByRole('list').nth(1)).toContainText(`email:can't be blank`);
-});
-
-test('Assert error message for wrong password', async ({ page }) => {
+  test('Assert error message for empty password', async ({ page }) => {
   await page.getByPlaceholder('Email').fill('test@gmail.com');
-  await page.getByPlaceholder('Password').fill('1');
   await page.getByRole('button', { name: 'Sign in' }).click();
- 
-  await expect(page.getByRole('list').nth(1)).toContainText(`email or password:is invalid`);
+
+  await expect(page.getByRole('list').nth(1)).toContainText(`password:can\'t be blank`);
+  });
+
+  test('Assert error message for empty email', async ({ page }) => {
+    await page.getByPlaceholder('Password').fill('newpass123!');
+    await page.getByRole('button', { name: 'Sign in' }).click();
+  
+    await expect(page.getByRole('list').nth(1)).toContainText(`email:can't be blank`);
+  });
+
+  test('Assert error message for wrong password', async ({ page }) => {
+    await page.getByPlaceholder('Email').fill('test@gmail.com');
+    await page.getByPlaceholder('Password').fill('1');
+    await page.getByRole('button', { name: 'Sign in' }).click();
+  
+    await expect(page.getByRole('list').nth(1)).toContainText(`email or password:is invalid`);
+  });
+
 });
